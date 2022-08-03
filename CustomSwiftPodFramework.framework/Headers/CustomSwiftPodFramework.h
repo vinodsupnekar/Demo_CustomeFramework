72c24a8131ca87bc03f9b63b646aedeeb4928356
vinodsupnekar <vinodsupnekar@users.noreply.github.com>
2022-08-03 23:00:42 +0530

    Add files via upload

---
+++ CustomSwiftPodFramework.framework/Headers/CustomSwiftPodFramework.h
@@ -0,0 +1,20 @@
+//
+//  CustomSwiftPodFramework.h
+//  CustomSwiftPodFramework
+//
+//  Created by vinods.alt on 03/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomSwiftPodFramework.
+FOUNDATION_EXPORT double CustomSwiftPodFrameworkVersionNumber;
+
+//! Project version string for CustomSwiftPodFramework.
+FOUNDATION_EXPORT const unsigned char CustomSwiftPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSwiftPodFramework/PublicHeader.h>
+#import <FirebaseCore/FirebaseCore.h>
+
+
+
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ex
+//
+//  Created by vinods.alt on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
